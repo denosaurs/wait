e4159e752aff8e7c37aa7315bb1e6c217d73ab7c
eliassjogreen <eliassjogreen1@gmail.com>
2020-11-21 02:52:23 +0100

    chore: bump deps

--- deps.ts
+++ deps.ts
@@ -1,3 +1,3 @@
-export * as colors from "https://deno.land/std@0.71.0/fmt/colors.ts";
-export { encode, decode } from "https://deno.land/std@0.71.0/encoding/utf8.ts";
-export * as tty from "https://deno.land/x/tty@0.1.0/mod.ts";
+export * as colors from "https://deno.land/std@0.78.0/fmt/colors.ts";
+export { encode, decode } from "https://deno.land/std@0.78.0/encoding/utf8.ts";
+export * as tty from "https://deno.land/x/tty@0.1.1/mod.ts";
