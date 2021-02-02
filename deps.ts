dc320fa7522f28d84333ae627d15dfd84e68e766
eliassjogreen <eliassjogreen1@gmail.com>
2021-02-02 19:00:50 +0100

    chore: bump deps

--- deps.ts
+++ deps.ts
@@ -1,3 +1,3 @@
-export * as colors from "https://deno.land/std@0.78.0/fmt/colors.ts";
-export { decode, encode } from "https://deno.land/std@0.78.0/encoding/utf8.ts";
-export * as tty from "https://deno.land/x/tty@0.1.1/mod.ts";
+export * as colors from "https://deno.land/std@0.85.0/fmt/colors.ts";
+export { decode, encode } from "https://deno.land/std@0.85.0/encoding/utf8.ts";
+export * as tty from "https://deno.land/x/tty@0.1.2/mod.ts";
